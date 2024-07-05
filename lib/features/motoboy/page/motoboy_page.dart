7d912c92bc9d3deedadf3f980241e48c1fadd0ef
rcdo-dev <rcdo.dev@gmail.com>
2024-07-04 23:12:36 -0300

    feat: motoboy page created.

---
+++ lib/features/motoboy/page/motoboy_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MotoboyPage extends StatelessWidget {
+  const MotoboyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Motoboy Page',
+        ),
+      ),
+    );
+  }
+}
