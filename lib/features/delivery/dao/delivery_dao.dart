ff4b1146bc6190a5814a365aad1167eada66543e
rcdo-dev <rcdo.dev@gmail.com>
2024-08-21 23:35:49 -0300

    feat: delivery dao class tested and implemented.

--- lib/features/delivery/dao/delivery_dao.dart
+++ lib/features/delivery/dao/delivery_dao.dart
@@ -1,10 +1,8 @@
+import 'package:sqflite/sqflite.dart';
+
 import 'package:boh_humm/core/data_access/connection_db/i_connection_db.dart';
 import 'package:boh_humm/core/data_access/dao/i_dao.dart';
 import 'package:boh_humm/features/delivery/model/delivery_model.dart';
-import 'package:boh_humm/main.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 class DeliveryDao implements IDao<DeliveryModel> {
   final IConnectionDb connectionDb;
@@ -117,74 +115,4 @@ class DeliveryDao implements IDao<DeliveryModel> {
     database.close();
     return null;
   }
-}
-
-void main() {
-  Modular.bindModule(AppModule());
-
-  late DeliveryModel delivery;
-  late IConnectionDb connection;
-  late DeliveryDao deliveryDao;
-
-  setUpAll(() {
-    // Initialize sqflite_common_ffi
-    sqfliteFfiInit();
-    // Tells sqflite to use the database factory provided by sqflite_common_ffi
-    databaseFactory = databaseFactoryFfi;
-
-    delivery = DeliveryModel(
-      del_order: 346,
-      del_fee: 11,
-      del_delr_id: 2,
-    );
-
-    connection = Modular.get<IConnectionDb>();
-    deliveryDao = DeliveryDao(connectionDb: connection);
-  });
-
-  tearDownAll(() {
-    print('All tests were carried out.');
-  });
-
-  group('Persistence delivery by Delivery class | ', () {
-    test('Must insert a delivery object into the database.', () async {
-      int? lastId = await deliveryDao.insert(data: delivery);
-      print(lastId);
-      expect(lastId, isNotNull);
-    });
-
-    test('Must return a list of delivery data.', () async {
-      List<Map>? list = await deliveryDao.getAll();
-      print(list);
-      expect(list, isNotEmpty);
-    });
-
-    test('Must return a delivery object using an id.', () async {
-      DeliveryModel? deliveryModel = await deliveryDao.getById(id: 4);
-      if (deliveryModel != null) {
-        print(
-          'Entrega id:\t${deliveryModel.del_id}\nComanda:\t${deliveryModel.del_order}\nTaxa:\t\t${deliveryModel.del_fee}\nRota id:\t${deliveryModel.del_delr_id}',
-        );
-      }
-      expect(deliveryModel, isA<DeliveryModel>());
-    });
-
-    test('Must update a Delivery object.', () async {
-      DeliveryModel deliveryUpdate = DeliveryModel(
-        del_id: 3,
-        del_order: 10,
-        del_fee: 6,
-      );
-      int? id = await deliveryDao.update(data: deliveryUpdate);
-      expect(id, greaterThan(0));
-    });
-
-    test('Must delete a Delivery object.', () async {
-      DeliveryModel deliveryDelete = DeliveryModel(
-        del_id: 6,
-      );
-      int? id = await deliveryDao.delete(data: deliveryDelete);
-      expect(id, greaterThan(0));
-    });
-  });
 }
